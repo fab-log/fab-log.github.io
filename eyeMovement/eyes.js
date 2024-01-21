769fceb4ca5a2086e5cd8e26e19308ff863bbcd7
fab-log <118112974+fab-log@users.noreply.github.com>
2024-01-21 15:19:38 +0100

    Create eyes.js

---
+++ eyeMovement/eyes.js
@@ -0,0 +1,19 @@
+const balls = document.getElementsByClassName('ball');
+// const balls = document.querySelectorAll('.ball');
+
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+  for (i = 0; i < balls.length; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+  }
+
+  // balls.forEach(e => {
+  //   e.style.left = x;
+  //   e.style.top = y;
+  //   e.transform = 'translate(-' + x + ',-' + y + ')';
+  // });
+};
